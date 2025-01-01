135f795a78942c7efd09035613cf9e1d7a1ef2eb
Sachidanand Prasad Sah <113513747+Sachidanand-sah@users.noreply.github.com>
2025-01-01 17:00:33 +0545

    change in App.css design

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 
 const TodoItem = ({ task, toggleTask, deleteTask }) => {
-  return (
-    <li className={`todo-item ${task.completed ? 'completed' : ''}`}>
-      <span>
-        <input
-          type="checkbox"
-          checked={task.completed}
-          onChange={() => toggleTask(task.id)}
-        />
-        {task.text}
-      </span>
-      <button onClick={() => deleteTask(task.id)}>Delete</button>
-    </li>
-  );
+    return (
+        <li className={`todo-item ${task.completed ? 'completed' : ''}`}>
+            <span>
+                <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => toggleTask(task.id)}
+                />
+                {task.text}
+            </span>
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+        </li>
+    );
+};
+
+TodoItem.propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        completed: PropTypes.bool.isRequired,
+    }).isRequired,
+    toggleTask: PropTypes.func.isRequired,
+    deleteTask: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
